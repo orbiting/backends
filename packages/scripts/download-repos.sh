02c2647c7fc0b74eb60ba355f2ccd11bb33020cd
Patrick Venetz <venetz@gmail.com>
2018-05-17 10:10:58 +0200

    feat(scripts): Allow download script to filter repo names before cloning

--- packages/scripts/download-repos.sh
+++ packages/scripts/download-repos.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
-set -eu;
+set -e;
 
 # Clone an organization's repositories into a folder.
+#
+# Usage:
+#   packages/scripts/download-repos.sh <source folder> (<name filter>)
+#
 # Example:
-#   packages/scripts/download-repos.sh ../repos
+#   packages/scripts/download-repos.sh ../repos .*
 
 if [ -z "$1" ] ; then
   echo "Provide path to clone repos into e.g. ../repos"
   exit 1
 fi
 
+if [ -z "$2" ] ; then
+  NAMEREGEXP=".*"
+else
+  NAMEREGEXP=$2
+fi
+
 BASEDIR=$(dirname "$0")
 
 DEBUG=scripts:* $BASEDIR/get-repos.js | \
+  grep "${NAMEREGEXP}" | \
   while read -r gitRepoUrl gitRepoName; \
   do \
     echo "${gitRepoName} cloning..."; \
-    git clone $gitRepoUrl $1/$gitRepoName --quiet; \
+    git clone $gitRepoUrl $1/$gitRepoName --mirror --quiet; \
     echo "${gitRepoName} cloned."; \
   done;
