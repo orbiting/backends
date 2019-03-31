7f411f944515eea20299d7ba29de01502c429d9b
Patrick Venetz <venetz@gmail.com>
2019-03-31 13:10:58 +0200

    feat(republik): Add payments.receivedAt, postfinancePayments.pspId
    Add additional fields to track date of payment reciepts

---
+++ servers/republik/migrations/sqls/20190331104535-postfinance-buchungsdatum-down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."postfinancePayments" DROP COLUMN IF EXISTS "pspId";
+ALTER TABLE "public"."payments" DROP COLUMN IF EXISTS "receivedAt";
---
+++ servers/republik/migrations/sqls/20190331104535-postfinance-buchungsdatum-up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."payments" ADD COLUMN "receivedAt" date;
+ALTER TABLE "public"."postfinancePayments" ADD COLUMN "pspId" text;
