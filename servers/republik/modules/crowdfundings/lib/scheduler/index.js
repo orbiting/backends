020fa6a58772c9d9dfd6fdad9ee0031065c7908a
Patrick Venetz <venetz@gmail.com>
2020-01-14 14:27:15 +0100

    feat(crowdfundings): Remove schedulers available in cronjob

--- servers/republik/modules/crowdfundings/lib/scheduler/index.js
+++ servers/republik/modules/crowdfundings/lib/scheduler/index.js
@@ -1,19 +1,7 @@
-const DEV = process.env.NODE_ENV && process.env.NODE_ENV !== 'production'
-
 const debug = require('debug')('crowdfundings:lib:scheduler')
-
-const {
-  intervalScheduler,
-  timeScheduler
-} = require('@orbiting/backend-modules-schedulers')
-
-const lockTtlSecs = 60 * 5 // 5 mins
-
-const { inform: informGivers } = require('./givers')
-const { inform: informCancellers } = require('./winbacks')
-const { run: membershipsOwnersHandler } = require('./owners')
-const { deactivate } = require('./deactivate')
-const { changeover } = require('./changeover')
+const PgDb = require('@orbiting/backend-modules-base/lib/PgDb')
+const Redis = require('@orbiting/backend-modules-base/lib/Redis')
+const { intervalScheduler } = require('@orbiting/backend-modules-schedulers')
 
 const surplus = require('../../../../graphql/resolvers/RevenueStats/surplus')
 const { populate: populateMembershipStatsEvolution } = require('../../../../lib/MembershipStats/evolution')
@@ -22,55 +10,17 @@ const countRange = require('../../../../graphql/resolvers/MembershipStats/countR
 const init = async (context) => {
   debug('init')
 
-  const schedulers = []
+  const pgdb = await PgDb.connect()
+  const redis = Redis.connect()
+  const context = {
+    ..._context,
+    pgdb,
+    redis
+  }
 
-  schedulers.push(
-    timeScheduler.init({
-      name: 'memberships-givers',
-      context,
-      runFunc: informGivers,
-      lockTtlSecs,
-      runAtTime: '06:00',
-      runInitially: DEV
-    })
-  )
-
-  schedulers.push(
-    intervalScheduler.init({
-      name: 'memberships-owners',
-      context,
-      runFunc: membershipsOwnersHandler,
-      lockTtlSecs,
-      runIntervalSecs: 60 * 10
-    })
-  )
-
-  schedulers.push(
-    timeScheduler.init({
-      name: 'winback',
-      context,
-      runFunc: informCancellers,
-      lockTtlSecs,
-      runAtTime: '18:32',
-      runAtDaysOfWeek: [1, 2, 3, 4, 5],
-      runInitially: DEV
-    })
-  )
-
-  schedulers.push(
-    intervalScheduler.init({
-      name: 'changeover-deactivate',
-      context,
-      runFunc: async (args, context) => {
-        await changeover(args, context)
-        await deactivate(args, context)
-      },
-      lockTtlSecs,
-      runIntervalSecs: 60 * 10
-    })
-  )
-
-  schedulers.push(
+  const schedulers = [
+    // stats-cache scheduler
+    // @TODO: Keep or move elsewhere
     intervalScheduler.init({
       name: 'stats-cache',
       context,
@@ -83,7 +33,7 @@ const init = async (context) => {
       lockTtlSecs: 10,
       runIntervalSecs: 60
     })
-  )
+  ]
 
   const close = async () => {
     await Promise.each(schedulers, scheduler => scheduler.close())
