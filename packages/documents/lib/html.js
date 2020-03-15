0ab1c57692b7bbc83c019206a3fac49bcc058b9f
patte <p@tte.io>
2020-03-15 23:30:36 +0100

    feat: support article email template

--- packages/documents/lib/html.js
+++ packages/documents/lib/html.js
@@ -1,11 +1,19 @@
 const newsletterEmailSchema = require('@project-r/template-newsletter/lib/email')
 const editorialNewsletterSchema = require('@project-r/styleguide/lib/templates/EditorialNewsletter/email')
+const articleSchema = require('@project-r/styleguide/lib/templates/Article/email')
 const { renderEmail } = require('mdast-react-render/lib/email')
 
 const get = doc => {
-  const emailSchema = doc.content.meta.template === 'editorialNewsletter'
-    ? editorialNewsletterSchema.default()  // Because styleguide currently doesn't support module.exports
-    : newsletterEmailSchema
+  const { template } = doc.content.meta
+
+  let emailSchema
+  if (template === 'editorialNewsletter') {
+    emailSchema = editorialNewsletterSchema.default() // Because styleguide currently doesn't support module.exports
+  } else if (template === 'article') {
+    emailSchema = articleSchema.default()
+  } else {
+    emailSchema = newsletterEmailSchema // PROJECT-R template
+  }
   return renderEmail(doc.content, emailSchema)
 }
 
