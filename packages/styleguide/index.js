9d025a3bd8dc972f184482792376eea6412ac668
Patrick Venetz <venetz@gmail.com>
2021-08-25 13:39:35 +0200

    fix(styleguide): Export article email schema

--- packages/styleguide/index.js
+++ packages/styleguide/index.js
@@ -1,11 +1,16 @@
-const commentSchema = require('@project-r/styleguide/lib/templates/Comment/email').default()
+const commentSchema =
+  require('@project-r/styleguide/lib/templates/Comment/email').default()
 const newsletterEmailSchema = require('@project-r/template-newsletter/lib/email')
 const editorialNewsletterSchema = require('@project-r/styleguide/lib/templates/EditorialNewsletter/email')
+const {
+  default: articleEmailSchema,
+} = require('@project-r/styleguide/lib/templates/Article/email')
 const { inQuotes } = require('@project-r/styleguide/lib/lib/inQuotes')
 
 module.exports = {
   commentSchema,
   newsletterEmailSchema,
   editorialNewsletterSchema,
+  articleEmailSchema,
   inQuotes,
 }
