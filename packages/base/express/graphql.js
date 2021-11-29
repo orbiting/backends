f29700559fcf042d1c72f96733eabad17f512663
Patrick Venetz <venetz@gmail.com>
2021-11-29 13:42:22 +0100

    wip

--- packages/base/express/graphql.js
+++ packages/base/express/graphql.js
@@ -1,14 +1,16 @@
 const { ApolloServer } = require('apollo-server-express')
+const { execute, subscribe, visit } = require('graphql')
 const { makeExecutableSchema } = require('graphql-tools')
-
+const { SubscriptionServer } = require('subscriptions-transport-ws')
 const cookie = require('cookie')
 const cookieParser = require('cookie-parser')
-const { transformUser } = require('@orbiting/backend-modules-auth')
 const util = require('util')
+
+const { transformUser } = require('@orbiting/backend-modules-auth')
 
 const { NODE_ENV, WS_KEEPALIVE_INTERVAL } = process.env
 
-module.exports = (
+module.exports = async (
   server,
   httpServer,
   pgdb,
@@ -39,22 +41,23 @@ module.exports = (
     return context
   }
 
-  const apolloServer = new ApolloServer({
-    schema: executableSchema,
-    context: ({ req, connection }) =>
-      connection ? connection.context : createContext({ user: req.user, req }),
-    debug: true,
-    introspection: true,
-    playground: false, // see ./graphiql.js
-    tracing: NODE_ENV === 'development',
-    subscriptions: {
-      onConnect: async (connectionParams, websocket) => {
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      // This is the `schema` we just created.
+      schema: executableSchema,
+      // These are imported from `graphql`.
+      execute,
+      subscribe,
+      // Providing `onConnect` is the `SubscriptionServer` equivalent to the
+      // `context` function in `ApolloServer`. Please [see the docs](https://github.com/apollographql/subscriptions-transport-ws#constructoroptions-socketoptions--socketserver)
+      // for more information on this hook.
+      async onConnect(connectionParams, webSocket, context) {
         try {
           // apollo-fetch used in tests sends cookie on the connectionParams
           const cookiesRaw =
             NODE_ENV === 'development' && connectionParams.cookies
               ? connectionParams.cookies
-              : websocket.upgradeReq.headers.cookie
+              : webSocket.upgradeReq.headers.cookie
           if (!cookiesRaw) {
             return createContext()
           }
@@ -84,8 +87,24 @@ module.exports = (
           throw new Error('error in subscriptions.onConnect')
         }
       },
-      keepAlive: WS_KEEPALIVE_INTERVAL || 40000,
+      keepAlive: WS_KEEPALIVE_INTERVAL || 1000 * 4,
     },
+    {
+      // This is the `httpServer` we created in a previous step.
+      server: httpServer,
+      // This `server` is the instance returned from `new ApolloServer`.
+      path: server.graphqlPath,
+    },
+  )
+
+  const apolloServer = new ApolloServer({
+    schema: executableSchema,
+    context: ({ req, connection }) =>
+      connection ? connection.context : createContext({ user: req.user, req }),
+    debug: true,
+    introspection: true,
+    playground: false, // see ./graphiql.js
+    tracing: NODE_ENV === 'development', // -> require('apollo-tracing').plugin()
     formatError: (error) => {
       console.log(
         `graphql error in ${this.operationName} (${JSON.stringify(
@@ -110,8 +129,66 @@ module.exports = (
       }
       return response
     },
+    validationRules: [(context) => {
+      console.log('validationRules', context)
+  
+      return {
+        Field: (node, key, parent, path, ancestors) => {
+          console.log('Field', node.alias?.value, node.name?.value, node.selectionSet, path, node)
+          return
+        }
+      }
+
+    }],
+    // You can import plugins or define them in-line, as shown:
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close()
+            },
+          }
+        },
+      },
+      /* {
+        async requestDidStart(initialRequestContext) {
+          return {
+            async executionDidStart(executionRequestContext) {
+              visit(executionRequestContext.operation, {
+                Field: (node, key, parent, path, ancestors) => {
+                  console.log('Field', key, path, node)
+                  return
+                }
+              })
+
+              // console.log(JSON.stringify(executionRequestContext.operation, null, 2))
+              return {
+                willResolveField({ source, args, context, info }) {
+                  const start = process.hrtime.bigint()
+                  return (error, result) => {
+                    const end = process.hrtime.bigint()
+                    console.log(
+                      `Field ${info.parentType.name}.${info.fieldName} took ${
+                        end - start
+                      }ns`,
+                    )
+                    if (error) {
+                      console.log(`It failed with ${error}`)
+                    } else {
+                      console.log(`It returned ${result}`)
+                    }
+                  }
+                },
+              }
+            },
+          }
+        },
+      }, */
+    ],
   })
 
+  await apolloServer.start()
   apolloServer.applyMiddleware({
     app: server,
     cors: false,
@@ -119,5 +196,4 @@ module.exports = (
       limit: '128mb',
     },
   })
-  apolloServer.installSubscriptionHandlers(httpServer)
 }
